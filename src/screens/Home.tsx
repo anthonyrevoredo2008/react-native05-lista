086825ff6de157bebf717203f536002b0cd2cf23
anthony2008 <anthonyrrafael@gmail.com>
2026-04-22 17:50:57 -0300

    atividade sidinei

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,19 +3,23 @@ import { View, Text, FlatList, Pressable } from 'react-native';
 
 import { styles } from './Styles';
 
+<Text style={styles.title}>
+            Lista de Frutas
+        </Text>
+
 const DATA = [
-    { id: "1", title: "Laranja" },
-    { id: "2", title: "Banana" },
-    { id: "3", title: "Morango" },
-    { id: "4", title: "Abacaxi" },
-    { id: "5", title: "Melancia" },
+    { id: "1", title: "Abacate", description: "Fruta rica em vitaminas e gorduras boas",  },
+    { id: "2", title: "Tangerina", description: "Fruta cítrica rica em vitamina C." },
+    { id: "3", title: "Uva", description: "Fruta pequena usada para vinhos e sucos." },
+    { id: "4", title: "Maracujá", description: "Fruta conhecida pelo efeito calmante." },
+    { id: "5", title: "Melão", description: "Fruta doce e refrescante." },
 ];
 
 export function Home() {
 
-    const renderItem = ({ item }: { item: { title: string } }) => (
+    const renderItem = ({ item }: { item: { title: string, description: string } }) => (
         <Pressable 
-            onPress={() => alert(item.title)} 
+            onPress={() => alert(`${item.title}\n\n${item.description}`)} 
             style={styles.item}
         >
             <Text style={styles.text}>{item.title}</Text>
@@ -24,6 +28,13 @@ export function Home() {
 
     return (
         <View style={styles.container}>
+
+
+
+    <Text style={styles.title}>
+    Lista de Frutas
+    </Text>
+
             <FlatList
                 numColumns={1}
                 style={styles.lista}
--- src/screens/Styles.tsx
+++ src/screens/Styles.tsx
@@ -2,28 +2,42 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1,                 // ocupa toda a tela
-    backgroundColor: '#f0f0f0', // centraliza horizontalmente
+    flex: 1,                 
+    backgroundColor: '#f0f0f0', 
   },
   lista: {
-    flex: 1,                 // FlatList ocupa toda altura
-    width: '100%',            // FlatList ocupa toda largura
-    backgroundColor: '#3c3333ff',
+    flex: 1,                
+    width: '100%',           
+    backgroundColor: '#87CEEB',
     paddingVertical: 30,
  },
   item: {
-    width: '90%',             // cada card ocupa 90% da tela
+    width: '20%',            
     padding: 20,
     marginTop: '5%',
-    margin:'auto',               // centraliza horizontalmente
-    backgroundColor: '#e9bd0bff',
-    alignItems: 'center',     // centraliza o conteúdo do card
+    margin:'auto',              
+    backgroundColor: '#90EE90',
+    alignItems: 'center',     
     borderWidth: 1,
     borderColor: '#f8c007ff',
-    borderRadius: 8,
+    borderRadius: 30,
   },
   text: {
     fontSize: 18,
     color: '#000',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 40,
+    marginBottom: 20,
+    color: '#000',
+  },
+  image: {
+    width: 80,
+    height: 80,
+    marginBottom: 10,
+    resizeMode: 'contain',
   },
 });
